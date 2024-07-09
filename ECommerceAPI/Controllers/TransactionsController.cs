c70e03b906d1e37b657653845971439fde57f9da
Patryk Tomczak <ptomczak98@gmail.com>
2024-07-09 21:08:58 +0200

    ADD TransactionController

---
+++ ECommerceAPI/Controllers/TransactionsController.cs
@@ -0,0 +1,99 @@
+[Route("api/[controller]")]
+[ApiController]
+public class TransactionsController : ControllerBase
+{
+    private readonly ApplicationDbContext _context;
+
+    public TransactionsController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    // GET: api/Transactions
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Transaction>>> GetTransactions()
+    {
+        return await _context.Transactions
+            .Include(t => t.Product)
+            .Include(t => t.Store)
+            .ToListAsync();
+    }
+
+    // GET: api/Transactions/5
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Transaction>> GetTransaction(int id)
+    {
+        var transaction = await _context.Transactions
+            .Include(t => t.Product)
+            .Include(t => t.Store)
+            .FirstOrDefaultAsync(t => t.TransactionId == id);
+
+        if (transaction == null)
+        {
+            return NotFound();
+        }
+
+        return transaction;
+    }
+
+    // PUT: api/Transactions/5
+    [HttpPut("{id}")]
+    public async Task<IActionResult> PutTransaction(int id, Transaction transaction)
+    {
+        if (id != transaction.TransactionId)
+        {
+            return BadRequest();
+        }
+
+        _context.Entry(transaction).State = EntityState.Modified;
+
+        try
+        {
+            await _context.SaveChangesAsync();
+        }
+        catch (DbUpdateConcurrencyException)
+        {
+            if (!TransactionExists(id))
+            {
+                return NotFound();
+            }
+            else
+            {
+                throw;
+            }
+        }
+
+        return NoContent();
+    }
+
+    // POST: api/Transactions
+    [HttpPost]
+    public async Task<ActionResult<Transaction>> PostTransaction(Transaction transaction)
+    {
+        _context.Transactions.Add(transaction);
+        await _context.SaveChangesAsync();
+
+        return CreatedAtAction("GetTransaction", new { id = transaction.TransactionId }, transaction);
+    }
+
+    // DELETE: api/Transactions/5
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteTransaction(int id)
+    {
+        var transaction = await _context.Transactions.FindAsync(id);
+        if (transaction == null)
+        {
+            return NotFound();
+        }
+
+        _context.Transactions.Remove(transaction);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    private bool TransactionExists(int id)
+    {
+        return _context.Transactions.Any(e => e.TransactionId == id);
+    }
+}
