9acfb66e5954b6247f72540a4d637cb567850605
Patryk Tomczak <ptomczak98@gmail.com>
2024-07-12 12:04:57 +0200

    ADD Database

--- my-app/src/components/UserForm.js
+++ my-app/src/components/UserForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import api from "/my-app/src/api.js";
+import api from './api';
 
 const UserForm = () => {
     const [username, setUsername] = useState('');
